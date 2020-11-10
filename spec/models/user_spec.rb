5cba74d9bf36419a66454485c4a0582c0823fbcf
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-11-11 07:49:49 +0900

    テストコードの記述と追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,28 +29,71 @@ RSpec.describe User, type: :model do
       it "emailに@が含まれていなければ登録できない" do
       end
       it "重複したemailがある場合、登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
       it "passwordが空では登録できない" do
         @user.password = ""
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it "passwordが5文字以下だと登録できない"
+      it "passwordが5文字以下だと登録できない" do
+        @user.password = "00000"
+        @user.password_confirmation = "00000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
       it "password_confirmationが空では登録できない" do
         @user.password_confirmation = ""
         @user.valid?
         expect(@user.errors.full_messsage).to include("Password confirmation doesn't match Password")
       end
-      it "last_nameが空だと登録できない"
+      it "last_nameが空だと登録できない" do
+        @user.last_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
-      it "first_nameが空だと登録できない"
+      it "last_nameが半角だと登録できない" do
+        
       end
-      it "last_name_kanaが空だと登録できない"
+      it "first_nameが空だと登録できない" do
+        @user.first_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
-      it "first_name_kanaが空だと登録できない"
+      it "first_nameが半角だと登録できない" do
+        
       end
-      it "birthdayが空だと登録できない"
+      it "last_name_kanaが空だと登録できない" do
+        @user.last_name_kana = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name(kana) can't be blank")
+      end
+      it "last_name_kanaが半角だと登録できない" do
+        
+      end
+      it "last_name_kanaが漢字・ひらがなだと登録できない" do
+        
+      end
+      it "first_name_kanaが空だと登録できない" do
+        @user.first_name_kana = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name(kana) can't be blank")
+      end
+      it "first_name_kanaが半角だと登録できない" do
+        
+      end
+      it "first_name_kanaが漢字・ひらがなだと登録できない" do
+        
+      end
+      it "birthdayが空だと登録できない" do
+        @user.birthday = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("birthday can't be blank")
+      
       end
     end 
   end
