f8c173f8c572ebb6dfcd8af55656347e2c629ec3
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-11-21 16:24:24 +0900

    ActiveStorageの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :category_id
+    validates :status_id
+    validates :ship_cost_id
+    validates :prefecture_id
+    validates :day_to_get_id
+    validates :price
+    validates :user
 end
