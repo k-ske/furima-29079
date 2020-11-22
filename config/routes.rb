5b4983ec7ac81bc327ad82198ea3c861d50a703d
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-11-22 14:04:52 +0900

    controllerの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-    get 'items/index'
-  root to: "items#index"
-  resources :items
+  root to: "products#index"
+  resources :products
   
 end
