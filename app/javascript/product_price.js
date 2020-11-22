2202f41689ce680155e06992b445b3e0fad9e9c5
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-11-22 15:54:41 +0900

    販売手数料と利益の表示

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -1,3 +1,11 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+window.addEventListener('load', function(){
+  const priceInput = document.getElementById("product-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const taxPrice = Math.floor(inputValue * 0.1);
+    document.getElementById("add-tax-price").innerHTML = taxPrice;
+    const profitPrice = Math.floor(inputValue * 0.9);
+    document.getElementById("profit").innerHTML = profitPrice;
+  })
+})
