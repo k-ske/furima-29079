f1a1d3447617f1672b7cb488c20745a728a056bc
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-11-28 13:52:29 +0900

    イベント発火の指定を変更

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('click', function(){
   const priceInput = document.getElementById("product-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
