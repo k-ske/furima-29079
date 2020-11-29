a2e996dcdb17af65a98af37ba1d4e1d2e57f9760
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-11-29 10:50:48 +0900

    before_actionの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -19,15 +20,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to root_path
     else
@@ -40,6 +38,10 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :content, :price, :image, :category_id, :status_id, :ship_cost_id, :prefecture_id, :day_to_get_id).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 end
