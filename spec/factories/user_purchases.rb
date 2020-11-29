367c1f36e7a375cd3b2ed60dff35879eddc763b3
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-11-29 23:56:00 +0900

    FactoryBotの設定とテストコードの追加

--- spec/factories/user_purchases.rb
+++ spec/factories/user_purchases.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user_purchase do
-    
+    postal_code         { '123-4567' }
+    prefecture_id       { 2 }
+    city                { '横浜市' }
+    house_number        { '1-1' }
+    building            { '横浜ハイツ' }
+    tel_nubmer          { '09012345678'}
+
+    association :purchase
   end
 end
--- spec/models/user_purchase_spec.rb
+++ spec/models/user_purchase_spec.rb
@@ -15,21 +15,19 @@ RSpec.describe UserPurchase, type: :model do
       end
       it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
       end
-      it 'prefectureを選択していないと保存できないこと' do
+      it 'prefecture_idが1だと登録できないこと' do
       end
-      it 'cityは空でも保存できること' do
+      it 'cityが空だと保存できないこと' do
       end
-      it 'house_numberは空でも保存できること' do
+      it 'house_numbeが空だと保存できないこと' do
       end
       it 'building_nameは空でも保存できること' do
       end
-      it 'priceが空だと保存できないこと' do
+      it 'tel_numberが空だと保存できないこと' do
       end
-      it 'priceが全角数字だと保存できないこと' do
+      it 'tel_numberにハイフンがあると保存できないこと' do
       end
-      it 'priceが1円未満では保存できないこと' do
-      end
-      it 'priceが1,000,000円を超過すると保存できないこと' do
+      it 'tel_numberが全角だと保存できないこと' do
       end
     end
   end
