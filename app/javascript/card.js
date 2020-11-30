1fa9c8954433761cab5ccc243aceb85735d6767e
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-12-01 01:14:06 +0900

    カード情報のトークン化

---
+++ app/javascript/card.js
@@ -0,0 +1,25 @@
+const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("user_purchase[number]"),
+      exp_month: formData.get("user_purchase[exp_month]"),
+      exp_year: `20${formData.get("user_purchase[exp_year]")}`,
+      cvc: formData.get("user_purchase[cvc]"),
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
