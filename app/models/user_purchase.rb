208ecb2767d4b7486a65c361910125fadf3ff8ce
k-ske <k-suke.fmb-1125@ezweb.ne.jp>
2020-12-01 07:01:31 +0900

    user_purchaseモデルにtokenの追加

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,6 +1,6 @@
 class UserPurchase
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :tel_number, :user_id, :product_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :tel_number, :user_id, :product_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
